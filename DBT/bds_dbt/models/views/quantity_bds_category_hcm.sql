2d3272fed00b6e843609474a7e450eea6e6254fe
pKietDE <phanmaiquockiet2004@gmail.com>
2024-11-19 12:27:56 +0000

    Fix upload error

--- DBT/bds_dbt/models/views/quantity_bds_category_hcm.sql
+++
@@ -1,5 +0,0 @@
-SELECT distinct {{ convert_category('bds.category_home') }} as category_home
-    , count(*) as quantity_bds_hcm    
-FROM {{ ref("dim_bds") }} as bds
-where bds.city = "hồ chí minh"
-GROUP BY {{ convert_category('bds.category_home') }}
